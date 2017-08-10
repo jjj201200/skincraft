1066e1cb08bedaebee732773cf3d01c32c4e8a07
Ruo <jjj201200@gmail.com>
2017-08-10 22:48:22 +0800

    update

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,7 +1,14 @@
 let path = require('path');
 let webpack = require('webpack');
+let node_modules_dir = path.join(__dirname, 'node_modules');
+let deps = [
+  'react/dist/react.min.js',
+  'react-router/dist/react-router.min.js',
+  'moment/min/moment.min.js',
+  'underscore/underscore-min.js',
+];
 
-module.exports = {
+let config = {
   entry: [
     'webpack/hot/only-dev-server',
     'webpack-dev-server/client?http://127.0.0.1:8080',
@@ -11,19 +18,16 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'src'),
     publicPath: '/src/',
-    filename: 'bundle.js'
+    filename: 'bundle.[hash].js'
   },
   devServer: {
     inline: true
   },
+  resolve: {
+    alias: {}
+  },
   module: {
-    // preLoaders: [
-    //   {
-    //     test: /\.jsx?/,
-    //     exclude: path.resolve(__dirname + '/node_modules'),
-    //     loader: 'jsxhint-loader'
-    //   }
-    // ],
+    noParse: [],
     loaders: [
       {
         test: /.js?$/,
@@ -33,22 +37,33 @@ module.exports = {
           presets: ['es2015', 'react']
         }
       }, {
-        test: /\.less/,
-        loader: 'style-loader!css-loader!less-loader'
+        test: /\.(css|less|scss)$/,
+        loader: 'style-loader!css-loader!less-loader!sass-loader'
       }, {
-        test: /\.(css)$/,
-        loader: 'style-loader!css-loader'
-      }, {
-        test: /\.(png|jpg|gif|svg)$/,
+        test: /\.(png|jpg|gif|svg|woff)$/,
         loader: 'file-loader?limit=8192'
       }, {
         test: /\.html$/,
         loader: 'html-loader'
+      }, {
+        test: path.resolve(node_modules_dir, deps[0]),
+        loader: "expose?React"
       }
     ]
   },
   plugins: [
     // new webpack.optimize.CommonsChunkPlugin(/* chunkName= */'vendor', /* filename= */'vendor.js'),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    // new webpack.optimize.UglifyJsPlugin({
+    //     compress: {
+    //         warnings: false
+    //     },
+    // }),
   ]
 };
+deps.forEach(function (dep) {
+  var depPath = path.resolve(node_modules_dir, dep);
+  config.resolve.alias[dep.split(path.sep)[0]] = depPath;
+  config.module.noParse.push(depPath);
+});
+module.exports = config;
